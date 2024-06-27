67d9023d98d6debc56adc41c88fc49723c2744cc
Chingis007 <karaulnekerell@gmail.com>
2024-06-27 16:48:09 +0300

    firstGo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [currentLink, setCurrentLink] = useState("About")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div id="left">
+        <div id="leftTop">
+          <div id="header">
+            <h1>Kyrylo Karaulnyi</h1>
+            <h2>Web Developer</h2>
+            <h3>
+              I build pixel-perfect, engaging, and accessible digital
+              experiences.
+            </h3>
+          </div>
+          <div id="mapLinks">
+            <div
+              id="linkDiv"
+              onClick={() => {
+                setCurrentLink("About")
+              }}
+            >
+              <div
+                id="line"
+                style={
+                  currentLink === "About"
+                    ? {
+                        backgroundColor: "white",
+                        width: "100px",
+                        color: "white",
+                      }
+                    : { display: "flex" }
+                }
+              ></div>
+              <p
+                style={
+                  currentLink === "About"
+                    ? { color: "white" }
+                    : { display: "flex" }
+                }
+              >
+                About
+              </p>
+            </div>
+            <div
+              id="linkDiv"
+              onClick={() => {
+                setCurrentLink("Projects")
+              }}
+            >
+              <div
+                id="line"
+                style={
+                  currentLink === "Projects"
+                    ? { borderColor: "white", width: "100px", color: "white" }
+                    : { display: "flex" }
+                }
+              ></div>
+              <p
+                style={
+                  currentLink === "Projects"
+                    ? { color: "white" }
+                    : { display: "flex" }
+                }
+              >
+                Projects
+              </p>
+            </div>
+          </div>
+        </div>
+        <div id="leftBottom">
+          <a href="https://github.com/Chingis007">
+            <img src="./github-mark.svg" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/kyrylo-karaulnyi-589950278/">
+            <img src="./linkedin.svg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div id="right">
+        <div>blabla</div>
+        <div>blabla</div>
+        <div>blabla</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
